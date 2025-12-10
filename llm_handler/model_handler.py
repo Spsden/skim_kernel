d5928c1318a9b1cab9ac6c17fb782ff31cf9db4e
Bhanupratap Singh <pratapsinghbhanu444@gmail.com>
2025-12-10 17:35:46 +0530

    feat, fix: implement llm call, code ref
    1. Model handler helps to call and summarize articles provided by caller fun

--- llm_handler/model_handler.py
+++ llm_handler/model_handler.py
@@ -2,69 +2,123 @@
 
 import logging
 import torch
+from typing import List, Optional
 from transformers import TorchAoConfig, AutoModelForSeq2SeqLM, AutoTokenizer, AutoModelForSequenceClassification
-from transformers import pipeline
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import pipeline, T5Model
+from transformers import T5Tokenizer, T5ForConditionalGeneration, AutoModelForCausalLM, pipeline
 
+from config.constants import model_config, prompts
 
 
 class ModelHandler:
 
     def __init__(self) -> None:
-        self.logger = logging.getLogger(f"LLM calling ")
+        self.logger = logging.getLogger("ModelHandler")
 
-        # init pipeline
+        # configs
+        self.model = model_config["name"]
+        self.model_token_size = model_config["token_size"]
+        self.chunk_size = model_config["chunk_size"]
+        self.model_type = model_config["type"]
+        self.device_map = model_config["device_map"]
+        
+        # prompts
+        self.chunk_prompt = prompts["chunking"]
+        self.article_summarizer_prompt = prompts["article_summarizer"]
+
+        # loggin
+        self.logger.info(f"Model: {self.model} \ntoken: {self.model_token_size} \nchunk_size: {self.chunk_size}")
+        
+
+        # load model
+        self.pipe = pipeline(self.model_type, model=self.model, device_map=self.device_map)
+        
+
+    def chunk_text(self, txt: str):
+        words = txt.split()
+
+        for i in range(0, len(words), self.chunk_size):
+            yield " ".join(words[i:i + self.chunk_size])
 
 
-        pass
 
+    def chunk_articles(self, articles: str) -> Optional[List[str]]:
+        """To chunk article into small strings if it exceed model token size
 
-    def get_articles_summarized(self):
-        pass
+        Args:
+            articles (str): complete article 
 
-    def init_pipline(self):
+        Returns:
+            Optional[List[str]]: chunked article in lst form
+        """
+        summaries = []
+
         try:
+            for chunk in self.chunk_text(articles):
 
-            chat = [
-                {"role": "system", "content": "You are a helpful science assistant."},
-                {"role": "user", "content": "Hey, can you explain gravity to me?"}
-            ]
+                prompt = f"{self.chunk_prompt}\n{chunk}"
+                output = self.pipe(prompt, do_sample=False, min_length=150, max_length=250)
+                summaries.append(output[0]['summary_text'])
 
-            tex = "hello how are you"
-            model_name = "google-t5/t5-base"
-            # model_name = "HuggingFaceTB/SmolLM2-1.7B-Instruct"
-            tokenizer = AutoTokenizer.from_pretrained(model_name)
-            pipeline1 = pipeline(task="text-generation", model=model_name, dtype="auto", device_map="auto")
-            response = pipeline(tex, max_new_tokens=512)
-            print(f"response is {response}")
-            # print(response[0]["generated_text"][-1]["content"])
-
-            # print(f"output is {output}")
-            # model = AutoModelForSeq2SeqLM.from_pretrained(
-            #     "google-t5/t5-base",
-            #     device_map="cpu",
-            #     torch_dtype=torch.float32,    # safest for CPU
-            # )
-
-            # tokenizer = AutoTokenizer.from_pretrained("google-t5/t5-base")
-
-            # inputs = tokenizer(
-            #     "translate English to French: The weather is nice today.",
-            #     return_tensors="pt"
-            # ).to("cpu")
-
-            # output = model.generate(**inputs)
-            # print(tokenizer.decode(output[0], skip_special_tokens=True))
+            return summaries
 
         except Exception as e:
-            self.logger.error(f"Error init_pipeline: {str(e)}")
+            self.logger.error(f"Error in chunking ${str(e)}")
+            return None
+
+    def summarize_article(self, article: str) -> Optional[str]:
+        """ summarizes article 
+
+        Args:
+            article (str): entire article 
+
+        Returns:
+            Optional[str]: summarized article
+        """
+        
+        try:
+            token_counts = self.get_tokens_count(article)
+
+            processsed_article: str = ""
+
+            # if token size of article is greater, chunk it
+            if token_counts > self.model_token_size:
+                # chunk article
+                chunked_articles = self.chunk_articles(article)
+
+                processsed_article = " ".join(chunked_articles)
+
+            else:
+                processsed_article = article
+                
+            prompt = f"{self.article_summarizer_prompt}\n {processsed_article}"
+
+            # summarize article
+            summarized_article = self.pipe(prompt, min_length=70, do_sample=False)
+
+            return summarized_article[0]['summary_text']
+            
+        except Exception as e:
+            self.logger.error(f"Summarization failed: {str(e)}")
+            return None
+        
+
+    def get_tokens_count(self, sentence: str) -> Optional[int]:
+
+        try:
+            tokenizer = AutoTokenizer.from_pretrained(self.model)
+
+            tokenized_input = tokenizer(sentence)
+
+            return len(tokenized_input["input_ids"])            
+
+        except Exception as e:
+            self.logger.error(f"Failed to count tokens: {str(e)}")
+            return None
 
 
 
 if __name__ == "__main__":
-    print("model hanlder")
+    print("hello world")
 
-    mod = ModelHandler()
-
-    mod.init_pipline()
     
